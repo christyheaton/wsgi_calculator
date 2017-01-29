656e434aef8a29bcb3a099239a26fd2e0c1ac3ab
Christy <christyheaton@gmail.com>
2017-01-29 13:31:25 -0800

    Update wsgi_calculator.py

--- wsgi_calculator.py
+++ wsgi_calculator.py
@@ -1,6 +1,6 @@
 '''
 http://localhost:8080/multiply/3/5  => 15
-http://localhost:8080/add/23/42     => 65
+http://localhost:8080/add/23/4/2     => 65
 http://localhost:8080/divide/6/0    => HTTP "400 Bad Request"
 '''
 def add(*args):
