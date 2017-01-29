50375aea8246c8e20bceb067308f889409c13e76
Christy Heaton <Christy Heaton>
2017-01-29 14:33:03 -0800

    added main page content

--- wsgi_calculator.py
+++ wsgi_calculator.py
@@ -1,8 +1,33 @@
+#!/usr/bin/env python
+
 '''
 http://localhost:8080/multiply/3/5  => 15
-http://localhost:8080/add/23/4/2     => 65
+http://localhost:8080/add/23/42     => 65
 http://localhost:8080/divide/6/0    => HTTP "400 Bad Request"
 '''
+page = """<html>
+<head>
+<title>WSGI Calulator!</title>
+</head>
+<body>
+<h1>Welcome to the WSGI Calculator!</h1>
+<p>Please type in a url like localhost:8080/functionname/operand/operand</p>
+It supports addition, subtraction, multiplication, and division
+<p>See the examples below:</p>
+<hr>
+<a href="http://localhost:8080/add/13/7">/add/13/7</a>
+<p><a href="http://localhost:8080/add/23/-42">/add/23/-42</a></p>
+<p><a href="http://localhost:8080/subtract/126/21">/subtract/126/21</a></p>
+<p><a href="http://localhost:8080/subtract/43/-780">/subtract/43/-780</a></p>
+<p><a href="http://localhost:8080/multiply/23/0">/multiply/23/0</a></p>
+<p><a href="http://localhost:8080/multiply/54/65">/multiply/54/65</a></p>
+<p><a href="http://localhost:8080/divide/60/15">/divide/60/15</a></p>
+<p><a href="http://localhost:8080/divide/16/0">/divide/16/0</a></p>
+<br>
+<h1>Current Answer: {result}</h1>
+</body>
+</html>"""
+
 def add(*args):
     return str(int(args[0]) + int(args[1]))
 
@@ -30,27 +55,28 @@ def resolve_path(path):
     return func, args
 
 def application(environ, start_response):
-	headers = [('Content-type', 'text/html')]
-	try:
-		path = environ.get('PATH_INFO', None)
-		if path is None:
-			raise NameError
-		func, args = resolve_path(path)
-		body = func(*args)
-		status = "200 OK"
-	except NameError:
-		status = "404 Not Found"
-		body = "<h1>Not Found</h1>"
-	except ZeroDivisionError:
-		status = "400 Bad Request"
-		body = "<h1>Only Chuck Norris can divide by zero.</h1>"
-	except Exception:
-		status = "500 Internal Server Error"
-		body = "<h1>Internal Server Error</h1>"
-	finally:
-		headers.append(('Content-length', str(len(body))))
-		start_response(status, headers)
-		return [body.encode('utf8')]
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        result = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        result = "Not Found"
+    except ZeroDivisionError:
+        status = "400 Bad Request"
+        result = "Only Chuck Norris can divide by zero"
+    except Exception:
+        status = "500 Internal Server Error"
+        result = "There is a problem with your url or some other error"
+    finally:
+        body = page.format(result=result)
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
